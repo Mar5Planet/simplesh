c273dc85677daf1388a55ffa2d2bd5fe5fa17413
Mar5Planet <amustaf@email.arizona.edu>
2021-02-18 21:26:05 -0500

    added frontend tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Conversation from './components/Conversation';
+import Message from './components/Message';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Conversation", () => {
+  it("should render a conversation", () => {
+    const wrapper = shallow(<Conversation key={'samplekey'} convo={{'id':1, 'title':'sample', 'created_at':'sample-time'}}/>);
+  });
+});
+
+describe("Message", () => {
+  it("should render a message with it's comments", () => {
+    const wrapper = shallow(<Message 
+      createThoughts={() => {'test'}} 
+      thoughts={[]}
+      key={'samplekey'}
+      message={{'id':1, 'conversation_id':1,'text':'sample', 'created_at':'2021-02-19T01:54:22.290Z'}}/>);
+  });
 });
