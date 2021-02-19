b25299d673db7e0292138ec604426f2c5e6170d5
Mar5Planet <amustaf@email.arizona.edu>
2021-02-19 10:19:34 -0500

    modified search

--- src/containers/Conversations.js
+++ src/containers/Conversations.js
@@ -5,12 +5,15 @@ const Conversations = (props) => {
     const [visibleForm, setVisibleForm] = useState(false)
     const [title, setTitle] = useState('')
     const [convos, setConvos] = useState(props.convos)
+    const [search, setSearch] = useState(false)
 
     const renderConvos = () => {
-        return convos.map(convo => <Conversation convo={convo} key={convo.id} />)
+        if (search) return convos.map(convo => <Conversation convo={convo} key={convo.id} />)
+        return props.convos.map(convo => <Conversation convo={convo} key={convo.id} />)
     }
 
     const handleSearch = term => {
+        if (term.length === 0) setSearch(false)
         let newConvos = props.convos.filter((convo) => convo.title.toLowerCase().includes(term.toLowerCase()))
         setConvos(newConvos)
     }
@@ -29,6 +32,7 @@ const Conversations = (props) => {
                     convo['title'] = title
                     props.createConvo(convo)
                     setVisibleForm(false)
+                    handleSearch('')
                     }
 
                 }}>
@@ -42,7 +46,8 @@ const Conversations = (props) => {
 
             <div className="search">
                 <h5>Search Conversation</h5>
-                <input onChange={(e) => handleSearch(e.target.value)} placeholder="Conversation title" />
+                <input onChange={(e) => {handleSearch(e.target.value)
+                setSearch(true)}} placeholder="Conversation title" />
             </div>
             {renderConvos()}
         </div>
